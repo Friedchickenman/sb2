4e99f966b5e4f6a69ae14ec81aa58a5951edaafb
Sungjun Park <psj12357@naver.com>
2025-04-13 04:51:22 +0900

    fourth

--- src/main/java/org/zerock/sb2/reply/entities/ReplyEntity.java
+++ src/main/java/org/zerock/sb2/reply/entities/ReplyEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.zerock.sb2.board.entities.BoardEntity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +17,9 @@ import lombok.ToString;
 
 @EntityListeners(value = AuditingEntityListener.class)
 @Entity
-@Table(name = "tbl_reply")
+@Table(name = "tbl_reply"  , indexes = {
+    @Index(name = "idx_board", columnList = "board_bno")
+  })
 @Getter
 @ToString
 @Builder
@@ -24,4 +27,22 @@ import lombok.ToString;
 @NoArgsConstructor
 public class ReplyEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    private String replyText;
+
+    private String replyer;
+
+    @ManyToOne
+    private BoardEntity board;
+
+    @CreatedDate
+  @Column(name = "regdate", updatable = false)
+  protected LocalDateTime regDate;
+
+  @LastModifiedDate
+  @Column(name ="moddate" )
+  protected LocalDateTime modDate;
 }
---
+++ src/test/java/org/zerock/sb2/reply/ReplyRepoTests.java
@@ -0,0 +1,15 @@
+package org.zerock.sb2.reply;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.sb2.reply.repository.ReplyRepository;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class ReplyRepoTests {
+    
+    @Autowired(required = false)
+    private ReplyRepository repository;
+}
