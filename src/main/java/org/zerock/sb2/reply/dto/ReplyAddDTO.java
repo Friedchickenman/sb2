906087b9667df2f20811d239f50aef85fb2688eb
Sungjun Park <psj12357@naver.com>
2025-04-14 04:00:00 +0900

    9

--- src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
+++ src/main/java/org/zerock/sb2/reply/dto/ReplyAddDTO.java
@@ -1,5 +1,26 @@
 package org.zerock.sb2.reply.dto;
 
+import java.time.LocalDateTime;
+
+import groovy.transform.builder.Builder;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@lombok.Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReplyAddDTO {
-    
+
+    @NotBlank
+    private String replyText;
+
+    @NotBlank
+    private String replyer;
+
+    @NotBlank
+    private Long bno;
+
 }
--- src/main/java/org/zerock/sb2/reply/service/ReplyServiceImpl.java
+++ src/main/java/org/zerock/sb2/reply/service/ReplyServiceImpl.java
@@ -2,6 +2,8 @@ package org.zerock.sb2.reply.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.sb2.reply.dto.ReplyAddDTO;
+import org.zerock.sb2.reply.entities.ReplyEntity;
 import org.zerock.sb2.reply.repository.ReplyRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +16,12 @@ import lombok.extern.log4j.Log4j2;
 public class ReplyServiceImpl implements ReplyService{
     
     private final ReplyRepository replyRepository;
+
+    @Override
+    public Long add(ReplyAddDTO addDTO) {
+
+        ReplyEntity entity = dtoToEntity(addDTO);
+        replyRepository.save(entity);
+        return entity.getRno();
+    }
 }
